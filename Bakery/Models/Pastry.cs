d786d06514fdae63c0b1e21258e2b0d66110e091
ChrisRDavila <crossdavila@gmail.com>
2024-01-19 15:26:28 -0800

    Test 3 for detemining total order value of 2$ pastry (no discount yet) based on quantity of pastries passed

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -3,9 +3,16 @@ namespace Bakery.Models
   public class Pastry
   {
     public int QuantityPastry { get; set; }
+    private int pastry_price = 0;
     public Pastry(int _quantity)
     {
       QuantityPastry = _quantity;
+    }
+
+    public int PastryPrice()
+    {
+      pastry_price = QuantityPastry * 2;
+      return pastry_price;
     }
   }
 }
