ca8ff23f5697aa12cfee279d5e8d1ac1e73a2045
ChrisRDavila <crossdavila@gmail.com>
2024-01-19 15:11:36 -0800

    Test created to get pastry quantity

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -9,8 +9,20 @@ namespace Bakery.Tests
     [TestMethod]
     public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
     {
-      Pastry testPastry = new Pastry();
+      Pastry testPastry = new Pastry(2);
       Assert.AreEqual(typeof(Pastry), testPastry.GetType());
+    }
+
+    [TestMethod]
+    public void GetPastryQuantity_ReturnsPastryOrder_Int()
+    {
+      //Arrange
+      int pastry_quantity = 2;
+      Pastry testPastry = new Pastry(pastry_quantity);
+      //Act
+      int result = testPastry.QuantityPastry;
+      //Assert
+      Assert.AreEqual(pastry_quantity, result);
     }
   }
 }
